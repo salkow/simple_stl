c8fc0b353955f28a05b4106011f1dfa58d768280
salkow <salkow@outlook.com>
2021-09-07 17:22:46 +0300

    my first commit

---
+++ src/Vec.hpp
@@ -0,0 +1,227 @@
+#ifndef VEC_HPP
+#define VEC_HPP
+
+#include <stdexcept>
+#include <type_traits>
+
+namespace Simple
+{
+
+const std::size_t DEFAULT_CAPACITY = 2;
+
+template <typename Vec>
+class VecIterator
+{
+public:
+	using value_type = typename Vec::value_type;
+	using pointer = value_type*;
+	using reference = value_type&;
+	using const_reference = value_type const&;
+	using size_type = std::size_t;
+
+	constexpr VecIterator(pointer ptr) : m_ptr(ptr) {}
+
+	constexpr VecIterator& operator++()
+	{
+		m_ptr++;
+		return *this;
+	}
+
+	constexpr VecIterator operator++(int)
+	{
+		VecIterator it = *this;
+		++(*this);
+		return it;
+	}
+
+	constexpr VecIterator& operator--()
+	{
+		m_ptr--;
+		return *this;
+	}
+
+	constexpr VecIterator operator--(int)
+	{
+		VecIterator it = *this;
+		--(*this);
+		return it;
+	}
+
+	constexpr VecIterator& operator+=(size_type offset)
+	{
+		m_ptr += offset;
+		return *this;
+	}
+
+	constexpr VecIterator& operator-=(size_type offset)
+	{
+		m_ptr -= offset;
+		return *this;
+	}
+
+	constexpr VecIterator operator+(size_type offset) const
+	{
+		VecIterator it = *this;
+		return it += offset;
+	}
+
+	constexpr VecIterator operator-(size_type offset) const
+	{
+		VecIterator it = *this;
+		return it -= offset;
+	}
+
+	constexpr reference operator[](size_type index) { return *(m_ptr + index); }
+	constexpr const_reference operator[](size_type index) const { return *(m_ptr + index); }
+
+	constexpr pointer operator->() { return m_ptr; }
+	constexpr pointer operator->() const { return m_ptr; }
+
+	constexpr reference operator*() { return *m_ptr; }
+	constexpr const_reference operator*() const { return *m_ptr; }
+
+	constexpr bool operator==(const VecIterator& other) const { return m_ptr == other.m_ptr; }
+	constexpr bool operator!=(const VecIterator& other) const { return !(*this == other); }
+	constexpr bool operator<(const VecIterator& other) const { return m_ptr < other.m_ptr; }
+	constexpr bool operator<=(const VecIterator& other) const { return m_ptr <= other.m_ptr; }
+	constexpr bool operator>(const VecIterator& other) const { return m_ptr > other.m_ptr; }
+	constexpr bool operator>=(const VecIterator& other) const { return m_ptr >= other.m_ptr; }
+
+private:
+	pointer m_ptr;
+};
+
+template <class T>
+class Vec
+{
+public:
+	using value_type = T;
+	using iterator = T*;
+	using const_iterator = T const*;
+	using pointer = T*;
+	using const_pointer = T const*;
+	using reference = value_type&;
+	using const_reference = value_type const&;
+	using size_type = std::size_t;
+
+	constexpr Vec(size_type capacity = DEFAULT_CAPACITY) :
+		m_capacity(capacity), m_elements(static_cast<T*>(::operator new(sizeof(T) * m_capacity)))
+	{
+	}
+
+	~Vec()
+	{
+		clear();
+		::operator delete(m_elements, m_capacity * sizeof(T));
+	}
+
+	constexpr void push_back(const T& value)
+	{
+		if (m_size >= m_capacity)
+			realloc(get_increased_capacity());
+
+		new (&m_elements[m_size]) T(value);
+
+		m_size++;
+	}
+
+	constexpr void push_back(T&& value)
+	{
+		if (m_size >= m_capacity)
+			realloc(get_increased_capacity());
+
+		new (&m_elements[m_size]) T(std::move(value));
+
+		m_size++;
+	}
+
+	template <typename... Args>
+	constexpr reference emplace_back(Args&&... args)
+	{
+		if (m_size >= m_capacity)
+			realloc(get_increased_capacity());
+
+		new (&m_elements[m_size]) T(std::forward<Args>(args)...);
+
+		return m_elements[m_size++];
+	}
+
+	constexpr void pop_back()
+	{
+		m_size--;
+		m_elements[m_size].~T();
+	}
+
+	constexpr reference operator[](size_type pos) { return m_elements[pos]; }
+	constexpr const_reference operator[](size_type pos) const { return m_elements[pos]; }
+
+	constexpr reference at(size_type pos)
+	{
+		if (pos >= m_size)
+			throw std::out_of_range("Index out of range");
+
+		return m_elements[pos];
+	}
+
+	constexpr const_reference at(size_type pos) const
+	{
+		if (pos >= m_size)
+			throw std::out_of_range("Index out of range");
+
+		return m_elements[pos];
+	}
+
+	constexpr void clear()
+	{
+		for (size_type i = 0; i < m_size; i++)
+			m_elements[i].~T();
+
+		m_size = 0;
+	}
+
+	constexpr iterator begin() { return iterator(m_elements); }
+	constexpr const_iterator begin() const { return iterator(m_elements); }
+
+	constexpr iterator end() { return iterator(m_elements + m_size); }
+	constexpr const_iterator end() const { return iterator(m_elements + m_size); }
+
+	constexpr reference front() { return m_elements[0]; }
+	constexpr const_reference front() const { return m_elements[0]; }
+
+	constexpr reference back() { return m_elements[m_size - 1]; }
+	constexpr const_reference back() const { return m_elements[m_size - 1]; }
+
+	constexpr pointer data() { return m_elements; }
+	constexpr const_pointer data() const { return m_elements; }
+
+	constexpr size_type size() const { return m_size; }
+	constexpr size_type capacity() const { return m_capacity; }
+	constexpr size_type empty() const { return m_size == 0; }
+
+private:
+	constexpr void realloc(size_type new_capacity)
+	{
+		auto new_block = static_cast<T*>(::operator new(new_capacity * sizeof(T)));
+
+		for (size_type i = 0; i < m_capacity; i++)
+		{
+			new (&new_block[i]) T(std::move(m_elements[i]));
+			m_elements[i].~T();
+		}
+
+		::operator delete(m_elements, m_capacity * sizeof(T));
+
+		m_elements = new_block;
+		m_capacity = new_capacity;
+	}
+
+	constexpr size_type get_increased_capacity() { return m_capacity + m_capacity / 2 + 1; }
+
+	size_type m_capacity; // Available space for elements.
+	size_type m_size = 0; // Number of elements.
+	T* m_elements;
+};
+
+} // namespace Simple
+
+#endif // VEC_HPP
---
+++ tests/tests-vec.cpp
@@ -0,0 +1,317 @@
+#include <catch2/catch.hpp>
+
+#include <string>
+#include "../src/Vec.hpp"
+
+using std::string;
+using namespace Simple;
+
+TEST_CASE("Non pointer tests", "[non_pointer_vec]")
+{
+	auto my_vec_1 = Vec<int>();
+	my_vec_1.push_back(5);
+	my_vec_1.push_back(6);
+	my_vec_1.emplace_back(7);
+
+	REQUIRE(my_vec_1.size() == 3);
+	REQUIRE(my_vec_1.capacity() == 4);
+
+	REQUIRE(my_vec_1[0] == 5);
+	REQUIRE(my_vec_1[1] == 6);
+	REQUIRE(my_vec_1[2] == 7);
+
+	my_vec_1[1] = 8;
+	REQUIRE(my_vec_1[1] == 8);
+
+	my_vec_1.pop_back();
+	REQUIRE(my_vec_1.size() == 2);
+
+	REQUIRE(my_vec_1[0] == 5);
+	REQUIRE(my_vec_1[1] == 8);
+
+	my_vec_1.clear();
+
+	REQUIRE(my_vec_1.size() == 0);
+
+	auto my_vec_2 = Vec<int>(2);
+	my_vec_2.push_back(15);
+	my_vec_2.push_back(16);
+	my_vec_2.push_back(17);
+	my_vec_2.push_back(18);
+
+	my_vec_2[1] = 26;
+	REQUIRE(my_vec_2[1] == 26);
+
+	REQUIRE(my_vec_2.size() == 4);
+	REQUIRE(my_vec_2.capacity() == 4);
+
+	my_vec_2.pop_back();
+	REQUIRE(my_vec_2.size() == 3);
+
+	REQUIRE(my_vec_2[0] == 15);
+	REQUIRE(my_vec_2[1] == 26);
+	REQUIRE(my_vec_2[2] == 17);
+
+	my_vec_2.clear();
+
+	REQUIRE(my_vec_2.size() == 0);
+}
+
+TEST_CASE("Pointer tests", "[non_pointer_vec]")
+{
+	auto my_vec_1 = Vec<int*>();
+	my_vec_1.push_back(new int(5));
+	my_vec_1.push_back(new int(6));
+	my_vec_1.emplace_back(new int(7));
+
+	REQUIRE(my_vec_1.size() == 3);
+	REQUIRE(my_vec_1.capacity() == 4);
+
+	REQUIRE(*(my_vec_1[0]) == 5);
+	REQUIRE(*(my_vec_1[1]) == 6);
+	REQUIRE(*(my_vec_1[2]) == 7);
+
+	delete my_vec_1[1];
+
+	my_vec_1[1] = new int(8);
+	REQUIRE(*(my_vec_1[1]) == 8);
+
+	delete my_vec_1[my_vec_1.size() - 1];
+	my_vec_1.pop_back();
+	REQUIRE(my_vec_1.size() == 2);
+
+	REQUIRE(*(my_vec_1[0]) == 5);
+	REQUIRE(*(my_vec_1[1]) == 8);
+
+	for (unsigned int i = 0; i < my_vec_1.size(); i++)
+	{
+		delete my_vec_1[i];
+	}
+
+	my_vec_1.clear();
+
+	REQUIRE(my_vec_1.size() == 0);
+
+	auto my_vec_2 = Vec<int*>(2);
+	my_vec_2.push_back(new int(15));
+	my_vec_2.push_back(new int(16));
+	my_vec_2.push_back(new int(17));
+	my_vec_2.push_back(new int(18));
+
+	delete my_vec_2[1];
+
+	my_vec_2[1] = new int(26);
+	REQUIRE(*(my_vec_2[1]) == 26);
+
+	REQUIRE(my_vec_2.size() == 4);
+	REQUIRE(my_vec_2.capacity() == 4);
+
+	delete my_vec_2[my_vec_2.size() - 1];
+
+	my_vec_2.pop_back();
+	REQUIRE(my_vec_2.size() == 3);
+
+	REQUIRE(*(my_vec_2[0]) == 15);
+	REQUIRE(*(my_vec_2[1]) == 26);
+	REQUIRE(*(my_vec_2[2]) == 17);
+
+	for (unsigned int i = 0; i < my_vec_2.size(); i++)
+	{
+		delete my_vec_2[i];
+	}
+
+	my_vec_2.clear();
+
+	REQUIRE(my_vec_2.size() == 0);
+}
+
+TEST_CASE("Iterator tests", "[iterator_vec]")
+{
+	{
+		auto my_vec = Vec<int>();
+
+		REQUIRE(my_vec.begin() == my_vec.end());
+
+		my_vec.push_back(1);
+
+		auto my_iter = my_vec.end();
+		my_iter--;
+
+		REQUIRE(my_iter == my_vec.begin());
+		REQUIRE(my_vec.begin() != my_vec.end());
+
+		my_vec.pop_back();
+
+		REQUIRE(my_vec.begin() == my_vec.end());
+
+		my_vec.push_back(1);
+		my_vec.emplace_back(2);
+
+		my_iter = my_vec.begin();
+		int value = *my_iter++;
+
+		REQUIRE(value == 1);
+
+		value = *my_iter--;
+		REQUIRE(value == 2);
+
+		my_vec.push_back(3);
+		my_vec.push_back(4);
+		my_vec.push_back(5);
+
+		my_iter = my_vec.begin();
+		value = my_iter[4];
+
+		REQUIRE(value == 5);
+
+		int counter = 0;
+		for (Vec<int>::iterator it = my_vec.begin(); it != my_vec.end(); it++)
+		{
+			counter++;
+		}
+
+		REQUIRE(counter == 5);
+
+		my_vec.pop_back();
+
+		counter = 0;
+		for (int iter_value : my_vec)
+		{
+			counter++;
+		}
+
+		REQUIRE(counter == 4);
+	}
+
+	{
+		auto my_vec = Vec<string>();
+
+		REQUIRE(my_vec.begin() == my_vec.end());
+
+		my_vec.push_back("1");
+
+		auto my_iter = my_vec.end();
+		my_iter--;
+
+		REQUIRE(my_iter == my_vec.begin());
+		REQUIRE(my_vec.begin() != my_vec.end());
+
+		my_vec.pop_back();
+
+		REQUIRE(my_vec.begin() == my_vec.end());
+
+		my_vec.push_back("1");
+		my_vec.push_back("2");
+
+		my_iter = my_vec.begin();
+		string value = *my_iter++;
+
+		REQUIRE(value == "1");
+
+		value = *my_iter--;
+		REQUIRE(value == "2");
+
+		my_vec.push_back("3");
+		my_vec.push_back("4");
+		my_vec.push_back("5");
+
+		my_iter = my_vec.begin();
+		value = my_iter[4];
+		REQUIRE(value == "5");
+
+		int counter = 0;
+		for (Vec<string>::iterator it = my_vec.begin(); it != my_vec.end(); it++)
+		{
+			counter++;
+		}
+
+		REQUIRE(counter == 5);
+
+		my_vec.pop_back();
+
+		counter = 0;
+		for (string iter_value : my_vec)
+		{
+			counter++;
+		}
+
+		REQUIRE(counter == 4);
+
+		my_iter = my_vec.begin();
+
+		my_iter += 2;
+
+		value = *my_iter;
+
+		REQUIRE(value == "3");
+
+		my_iter -= 2;
+
+		value = *my_iter;
+
+		REQUIRE(value == "1");
+
+		my_iter = my_iter + 1;
+
+		value = *my_iter;
+
+		REQUIRE(value == "2");
+
+		my_iter = my_iter - 1;
+
+		value = *my_iter;
+
+		REQUIRE(value == "1");
+
+		REQUIRE(my_vec.at(1) == "2");
+
+		REQUIRE(my_vec.front() == "1");
+		REQUIRE(my_vec.back() == "4");
+
+		std::string* data = my_vec.data();
+		REQUIRE(data[2] == "3");
+	}
+}
+
+TEST_CASE("push_back, emplace_back", "[push_back_emplace_back]")
+{
+	{
+		auto my_vec = Vec<int>();
+
+		my_vec.push_back(111111);
+		my_vec.emplace_back(222222);
+		my_vec.push_back(333333);
+
+		REQUIRE(my_vec.at(1) == 222222);
+
+		my_vec.pop_back();
+
+		REQUIRE(my_vec.back() == 222222);
+
+		my_vec.clear();
+
+		my_vec.push_back(234);
+
+		REQUIRE(my_vec.front() == 234);
+	}
+
+	{
+		auto my_vec = Vec<string>();
+
+		my_vec.push_back(string("111111111111111111111111111111111111111111111111"));
+		my_vec.emplace_back("22222222222222222222222222222222222222222222222222222222");
+		my_vec.push_back(string("33333333333333333333333333333333333333333333333333333333"));
+
+		REQUIRE(my_vec.at(1) == "22222222222222222222222222222222222222222222222222222222");
+
+		my_vec.pop_back();
+
+		REQUIRE(my_vec.back() == "22222222222222222222222222222222222222222222222222222222");
+
+		my_vec.clear();
+
+		my_vec.push_back(string("2, 3, 4"));
+
+		REQUIRE(my_vec.front() == "2, 3, 4");
+	}
+}
