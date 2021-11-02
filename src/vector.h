db614dbaf4bea93964d2bf0a94c784469975aefa
salkow <salkow@outlook.com>
2021-11-02 11:58:57 +0200

    Removed main function from src and used enable_if_t instead of enable_if.

--- src/vector.h
+++ src/vector.h
@@ -258,16 +258,16 @@ private:
 	}
 
 	template <typename X>
-	typename std::enable_if<!std::is_nothrow_move_constructible<X>::value>::
-		type constexpr transfer_items_to_new_block(X* new_block) const
+	typename std::enable_if_t<!std::is_nothrow_move_constructible<
+		X>::value> constexpr transfer_items_to_new_block(X* new_block) const
 	{
 		for (size_type i = 0; i < m_size; ++i)
 			new (&new_block[i]) T(m_elements[i]);
 	}
 
 	template <typename X>
-	typename std::enable_if<std::is_nothrow_move_constructible<X>::value>::
-		type constexpr transfer_items_to_new_block(X* new_block)
+	typename std::enable_if_t<std::is_nothrow_move_constructible<
+		X>::value> constexpr transfer_items_to_new_block(X* new_block)
 	{
 		for (size_type i = 0; i < m_size; ++i)
 			new (&new_block[i]) T(std::move(m_elements[i]));
