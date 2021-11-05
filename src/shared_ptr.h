6a7bb0e8ff6d0345e4336583e9bcd436f7da6ad3
salkow <salkow@outlook.com>
2021-11-05 23:06:35 +0200

    Did a basic start on a shared_ptr.

---
+++ src/shared_ptr.h
@@ -0,0 +1,88 @@
+#ifndef SHARED_PTR_H
+#define SHARED_PTR_H
+
+#include <utility>
+
+namespace simple
+{
+
+template <typename T>
+class shared_ptr
+{
+public:
+	using pointer = T*;
+
+	constexpr shared_ptr() noexcept = default;
+	constexpr explicit shared_ptr(std::nullptr_t) noexcept {}
+
+	constexpr explicit shared_ptr(T* p) : m_data(p), m_count(new int64_t(1)) {}
+
+	shared_ptr(shared_ptr&& u) noexcept :
+		m_data(std::exchange(u.m_data, nullptr)), m_count(std::exchange(u.m_count, nullptr))
+	{
+	}
+
+	shared_ptr& operator=(shared_ptr&& u) noexcept
+	{
+		decrement_and_delete_if_last();
+
+		m_data = std::exchange(u.m_data, nullptr);
+		m_count = std::exchange(u.m_count, nullptr);
+		return *this;
+	}
+
+	shared_ptr(const shared_ptr& p) : m_data(p.m_data), m_count(p.m_count)
+	{
+		if (m_count)
+			++(*m_count);
+	}
+
+	shared_ptr& operator=(const shared_ptr& p)
+	{
+		if (this == &p || m_count == p.m_count)
+			return *this;
+
+		decrement_and_delete_if_last();
+
+		m_data = p.m_data;
+		m_count = p.m_count;
+
+		if (m_count)
+			(*m_count)++;
+
+		return *this;
+	}
+
+	~shared_ptr() { decrement_and_delete_if_last(); }
+
+	T& operator*() const noexcept { return *m_data; }
+	pointer operator->() const noexcept { return m_data; }
+
+	explicit operator bool() const noexcept { return m_data; }
+
+	[[nodiscard]] int64_t use_count() const noexcept { return m_count ? *m_count : 0; }
+
+	[[nodiscard]] pointer get() const noexcept { return m_data; }
+
+private:
+	void decrement_and_delete_if_last()
+	{
+		if (!m_count)
+			return;
+
+		--(*m_count);
+
+		if (*m_count == 0)
+		{
+			delete m_data;
+			delete m_count;
+		}
+	}
+
+	T* m_data = nullptr;
+	int64_t* m_count = nullptr;
+};
+
+} // namespace simple
+
+#endif // SHARED_PTR_H
---
+++ tests/tests-shared_ptr.cpp
@@ -0,0 +1,95 @@
+#include "../lib/include/catch2/catch.hpp"
+#include "../src/shared_ptr.h"
+
+#include <string>
+
+using simple::shared_ptr;
+using std::string;
+
+TEST_CASE("Construct empty shared_ptr", "[construct_empty_shared_ptr]")
+{
+	shared_ptr<string> x_1;
+	REQUIRE(!x_1.get());
+
+	shared_ptr<string> x_2;
+	REQUIRE(!x_2);
+}
+
+TEST_CASE("Construct shared_ptr", "[construct_shared_ptr]")
+{
+	shared_ptr<string> x(new string("Very very very very long string....."));
+
+	REQUIRE(*x == string("Very very very very long string....."));
+}
+
+TEST_CASE("shared_ptr simple move operations", "[shared_simple_ptr_move_operations]")
+{
+	shared_ptr<int> x_1(new int(4));
+	REQUIRE(*x_1 == 4);
+	REQUIRE(x_1.use_count() == 1);
+
+	shared_ptr<int> x_2(std::move(x_1));
+	REQUIRE(*x_2 == 4);
+	REQUIRE(x_2.use_count() == 1);
+
+	shared_ptr<int> x_3;
+	x_3 = std::move(x_2);
+	REQUIRE(*x_3 == 4);
+
+	shared_ptr<int> x_4(std::move(x_3));
+	x_4 = shared_ptr<int>(new int(15));
+	REQUIRE(*x_4 == 15);
+	REQUIRE(*(x_4.get()) == 15);
+}
+
+TEST_CASE("shared_ptr simple copy operations", "[shared_ptr_simple_copy_operations]")
+{
+	shared_ptr<int> x_1(new int(4));
+	REQUIRE(*x_1 == 4);
+	REQUIRE(x_1.use_count() == 1);
+
+	shared_ptr<int> x_2(x_1);
+	REQUIRE(*x_2 == 4);
+	REQUIRE(x_1.use_count() == 2);
+	REQUIRE(x_2.use_count() == 2);
+	REQUIRE(x_1);
+
+	shared_ptr<int> x_3;
+	x_3 = x_2;
+	REQUIRE(*x_3 == 4);
+	REQUIRE(x_3.use_count() == 3);
+
+	shared_ptr<int> x_4(x_3);
+	REQUIRE(x_4.use_count() == 4);
+
+	shared_ptr<int> x_5(new int(9));
+	x_4 = x_5;
+	REQUIRE(*x_4 == 9);
+	REQUIRE(*(x_4.get()) == 9);
+	REQUIRE(x_4.use_count() == 2);
+	REQUIRE(x_3.use_count() == 3);
+}
+
+TEST_CASE("shared_ptr copy/move empty object", "[shared_ptr_copy_move_empty_object]")
+{
+	shared_ptr<int> x_1;
+	shared_ptr<int> x_2(x_1);
+
+	shared_ptr<int> x_3;
+	x_3 = x_2;
+
+	shared_ptr<int> x_4(std::move(x_3));
+
+	REQUIRE(!x_1);
+	REQUIRE(x_1.use_count() == 0);
+
+	REQUIRE(!x_2);
+	REQUIRE(!x_4.get());
+}
+
+TEST_CASE("shared_ptr self assignment in 2 steps", "[shared_ptr_self_assignment_2_steps]")
+{
+	shared_ptr<int> x_1(new int(11));
+	shared_ptr<int> x_2(x_1);
+	x_1 = x_2;
+}
