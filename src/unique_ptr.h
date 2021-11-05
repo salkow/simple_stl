03b188db7594dcaa8e66bfaf708bc8450d184db4
salkow <salkow@outlook.com>
2021-11-05 10:57:42 +0200

    Created a unique_ptr class with some basic functionality and some tests.

---
+++ src/unique_ptr.h
@@ -0,0 +1,62 @@
+#ifndef UNIQUE_PTR_H
+#define UNIQUE_PTR_H
+
+#include <utility>
+
+namespace simple
+{
+
+template <typename T>
+class unique_ptr
+{
+public:
+	using pointer = T*;
+
+	constexpr unique_ptr() noexcept = default;
+	explicit unique_ptr(pointer p) : m_data(p) {}
+	constexpr explicit unique_ptr(std::nullptr_t) noexcept {}
+
+	unique_ptr(unique_ptr&& u) noexcept
+	{
+		reset(u.get());
+		u.m_data = nullptr;
+	}
+
+	unique_ptr& operator=(unique_ptr&& u) noexcept
+	{
+		reset(u.get());
+		u.m_data = nullptr;
+	}
+
+	unique_ptr& operator=(std::nullptr_t) noexcept
+	{
+		reset();
+		return *this;
+	}
+
+	unique_ptr(const unique_ptr& p) = delete;
+	T& operator=(const unique_ptr& p) = delete;
+
+	~unique_ptr() { delete m_data; }
+
+	pointer get() const noexcept { return m_data; }
+
+	void reset(pointer ptr = pointer()) noexcept
+	{
+		delete m_data;
+		m_data = ptr;
+	}
+
+private:
+	void move(unique_ptr&& u)
+	{
+		reset(u.get());
+		u.m_data = nullptr;
+	}
+
+	T* m_data = nullptr;
+};
+
+} // namespace simple
+
+#endif // UNIQUE_PTR_H
---
+++ tests/tests-unique_ptr.cpp
@@ -0,0 +1,36 @@
+#include "../lib/include/catch2/catch.hpp"
+
+#include "../src/unique_ptr.h"
+
+#include <utility>
+
+using simple::unique_ptr;
+
+TEST_CASE("Construct empty unique_ptr", "[construct_empty_unique_ptr]")
+{
+	unique_ptr<int> empty_1;
+	unique_ptr<int> empty_2(nullptr);
+}
+
+TEST_CASE("Construct unique_ptr", "[construct_unique_ptr]") { unique_ptr<int> x(new int(3)); }
+
+TEST_CASE("Move operations", "[move_operations")
+{
+	unique_ptr<int> x_1(new int(4));
+
+	unique_ptr<int> x_2(std::move(x_1));
+
+	unique_ptr<int> x_3 = std::move(x_2);
+}
+
+TEST_CASE("Reset unique_ptr", "[reset_unique_ptr]")
+{
+	unique_ptr<int> x(new int(5));
+
+	x.reset(new int(3));
+
+	x.reset();
+
+	unique_ptr<int> x_1(new int(7));
+	x_1 = nullptr;
+}
